dc1285d304cb2e92fe68cff48ee3fc7e19930736
elchibek1 <elchi.work00@gmail.com>
2022-12-21 16:25:04 +0600

    Create product controller and add route for this contoller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\ProductsController::class, 'index'])->name('home');
+Route::resource('/products', [\App\Http\Controllers\ProductsController::class]);
