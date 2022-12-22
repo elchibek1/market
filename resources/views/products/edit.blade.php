52e91a7ba8c7203d2ba6331c70ed268fe5fbab97
elchibek1 <elchi.work00@gmail.com>
2022-12-22 16:23:35 +0600

    work with edit view. this view code looks like create view, differences only which route will refer

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('base')
+@section('content')
+    <h1 class="mb-3">Edit product</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{route('products.update', ['product' => $product])}}" method="post">
+        @method('put')
+        @csrf
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{$product->title}}">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" name="description" id="description">{{$product->description}}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input type="text" name="price" class="form-control" value="{{$product->price}}">
+        </div>
+        {{--        <div class="form-group">--}}
+        {{--            <label for="category"></label>--}}
+        {{--        </div>--}}
+        <button type="submit" class="btn btn-primary mt-5">Submit</button>
+    </form>
+
+@endsection
